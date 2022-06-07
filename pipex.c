08b87d556150b7f1faed424a269bf60cd21fe5c5
belenarbizu <100694123+belenarbizu@users.noreply.github.com>
2022-06-07 17:11:10 +0200

    Update pipex.c

--- pipex.c
+++ pipex.c
@@ -1,101 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: barbizu- <barbizu-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/26 11:15:13 by barbizu-          #+#    #+#             */
+/*   Updated: 2022/06/07 17:09:54 by barbizu-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "pipex.h"
+#include <stdio.h>
 
-void	ft_error(char *str)
+void	ft_paths_arg(char **paths_sep, char **arg, char **envp)
 {
-	perror(str);
-	exit(0);
+	char	*temp;
+	char	*cmd;
+	int		i;
+
+	i = 0;
+	while (paths_sep[i])
+	{
+		temp = ft_strjoin(paths_sep[i], "/");
+		cmd = ft_strjoin(temp, arg[0]);
+		free(temp);
+		execve(cmd, arg, envp);
+		if (!access(cmd, F_OK))
+			ft_error("Error en Comando");
+		free(cmd);
+		i++;
+	}
 }
 
 void	ft_child_process(int *fd, int *file, char **argv, char **envp)
 {
-	int		i;
-	char	*cmd;
+	char	**arg;
 	char	*path_env;
 	char	**paths_sep;
-	char	**arg;
 
 	path_env = ft_find_path(envp);
 	paths_sep = ft_split(path_env, ':');
 	arg = ft_split(argv[2], ' ');
-	dup2(file[0], 0);
-	dup2(fd[1], 1);
 	close(fd[0]);
-	close(file[0]);
-	i = 0;
-	while (paths_sep[i])
-	{
-		cmd = ft_strjoin(paths_sep[i], arg[0]);
-		if (!access(cmd, X_OK))
-			ft_error("Error en Comando");
-		execve(cmd, arg, envp);
-		//ft_error("Error en Execve");
-		//free(cmd);
-		i++;
-	}
+	dup2(file[0], STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[1]);
+	ft_paths_arg(paths_sep, arg, envp);
 }
 
 void	ft_parent_process(int *fd, int *file, char **argv, char **envp)
 {
-	int		status;
-	int		i;
-	char	*temp;
-	char	*cmd;
+	char	**arg;
 	char	*path_env;
 	char	**paths_sep;
-	char	**arg;
 
-	if (waitpid(-1, &status, 0) == -1)
-		ft_error("Error en Waitpid");
 	path_env = ft_find_path(envp);
 	paths_sep = ft_split(path_env, ':');
 	arg = ft_split(argv[3], ' ');
-	dup2(file[1], 1);
-	dup2(fd[0], 0);
 	close(fd[1]);
-	close(file[1]);
-	i = 0;
-	while (paths_sep[i])
-	{
-		temp = ft_strjoin(paths_sep[i], '/');
-		cmd = ft_strjoin(temp, arg[0]);
-		free(temp);
-		if (!access(cmd, F_OK))
-			ft_error("Error en Comando");
-		execve(cmd, arg, envp);
-		//ft_error("Error en Execve");
-		//free(cmd);
-		i++;
-	}
+	dup2(file[1], STDOUT_FILENO);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+	ft_paths_arg(paths_sep, arg, envp);
 }
 
-char	*ft_find_path(char **envp)
+void	ft_pipex(int *fd, char **argv, char **envp)
 {
-	int		i;
-	char	*path_env;
+	int		file[2];
+	pid_t	child_pid;
 
-	i = 0;
-	while (!ft_strnstr(envp[i], "PATH=", 5))
-		i++;
-	path_env = ft_strnstr(envp[i], "PATH=", 5);
-	return (path_env + 5);
-}
-
-int	main(int argc, char **argv, char **envp)
-{
-	int			fd[2];
-	int			file[2];
-	pid_t		child_pid;
-
-	if (argc != 5)
-		ft_error("Error en Argc");
-	if (pipe(fd) < 0)
-		ft_error("Error en Pipe");
 	file[0] = open(argv[1], O_RDONLY);
-	if (file[0] == -1)
-		ft_error("Error en Child File");
 	file[1] = open(argv[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
-	if (file[1] == -1)
-		ft_error("Error en Parent File");
+	if (file[0] == -1 || file[1] == -1)
+		ft_error("Error en File");
 	child_pid = fork();
 	if (child_pid == -1)
 		ft_error("Error en Fork");
@@ -103,4 +80,17 @@ int	main(int argc, char **argv, char **envp)
 		ft_child_process(fd, file, argv, envp);
 	else
 		ft_parent_process(fd, file, argv, envp);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int			fd[2];
+	int			status;
+
+	if (argc != 5)
+		ft_error("Error en Argc");
+	if (pipe(fd) < 0)
+		ft_error("Error en Pipe");
+	ft_pipex(fd, argv, envp);
+	waitpid(-1, &status, 0);
 }
