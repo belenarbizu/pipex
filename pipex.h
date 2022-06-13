32894553fb384fbf0482690c0fde26a469963899
belenarbizu <100694123+belenarbizu@users.noreply.github.com>
2022-06-13 10:13:57 +0200

    Update pipex.h

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: barbizu- <barbizu-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 11:26:15 by barbizu-          #+#    #+#             */
-/*   Updated: 2022/06/07 16:11:38 by barbizu-         ###   ########.fr       */
+/*   Updated: 2022/06/09 11:14:36 by barbizu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PIPEX_H
@@ -18,11 +18,12 @@
 # include <fcntl.h>
 # include "libft/libft.h"
 
-void	ft_error(char *str);
-void	ft_paths_arg(char **paths_sep, char **arg, char **envp);
+char	*ft_paths_arg(char **paths_sep, char **arg, char **envp);
 void	ft_child_process(int *fd, int *file, char **argv, char **envp);
 void	ft_parent_process(int *fd, int *file, char **argv, char **envp);
 char	*ft_find_path(char **envp);
 void	ft_pipex(int *fd, char **argv, char **envp);
+void	free_matrix(char **matrix);
+void	ft_error(char *str);
 
 #endif
