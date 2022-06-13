60ad25df7bd194aa2bf080b84b796a5bbf678f65
belenarbizu <100694123+belenarbizu@users.noreply.github.com>
2022-06-13 10:13:24 +0200

    Update pipex.c

--- pipex.c
+++ pipex.c
@@ -6,30 +6,30 @@
 /*   By: barbizu- <barbizu-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 11:15:13 by barbizu-          #+#    #+#             */
-/*   Updated: 2022/06/07 17:09:54 by barbizu-         ###   ########.fr       */
+/*   Updated: 2022/06/09 11:30:28 by barbizu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "pipex.h"
-#include <stdio.h>
 
-void	ft_paths_arg(char **paths_sep, char **arg, char **envp)
+char	*ft_paths_arg(char **paths_sep, char **arg, char **envp)
 {
 	char	*temp;
 	char	*cmd;
 	int		i;
 
 	i = 0;
+	(void)envp;
 	while (paths_sep[i])
 	{
 		temp = ft_strjoin(paths_sep[i], "/");
 		cmd = ft_strjoin(temp, arg[0]);
 		free(temp);
-		execve(cmd, arg, envp);
 		if (!access(cmd, F_OK))
-			ft_error("Error en Comando");
+			return (cmd);
 		free(cmd);
 		i++;
 	}
+	return (NULL);
 }
 
 void	ft_child_process(int *fd, int *file, char **argv, char **envp)
@@ -37,15 +37,28 @@ void	ft_child_process(int *fd, int *file, char **argv, char **envp)
 	char	**arg;
 	char	*path_env;
 	char	**paths_sep;
+	char	*cmd;
 
 	path_env = ft_find_path(envp);
 	paths_sep = ft_split(path_env, ':');
 	arg = ft_split(argv[2], ' ');
 	close(fd[0]);
-	dup2(file[0], STDIN_FILENO);
-	dup2(fd[1], STDOUT_FILENO);
+	close(file[1]);
+	if (dup2(file[0], STDIN_FILENO) == -1)
+		ft_error("Error en Dup");
+	if (dup2(fd[1], STDOUT_FILENO) == -1)
+		ft_error("Error en Dup");
 	close(fd[1]);
-	ft_paths_arg(paths_sep, arg, envp);
+	close(file[0]);
+	cmd = ft_paths_arg(paths_sep, arg, envp);
+	if (!cmd)
+	{
+		free_matrix(paths_sep);
+		ft_putstr_fd("command not found: ", 2);
+		ft_putendl_fd(argv[2], 2);
+		free(cmd);
+	}
+	execve(cmd, arg, envp);
 }
 
 void	ft_parent_process(int *fd, int *file, char **argv, char **envp)
@@ -53,15 +66,28 @@ void	ft_parent_process(int *fd, int *file, char **argv, char **envp)
 	char	**arg;
 	char	*path_env;
 	char	**paths_sep;
+	char	*cmd;
 
 	path_env = ft_find_path(envp);
 	paths_sep = ft_split(path_env, ':');
 	arg = ft_split(argv[3], ' ');
 	close(fd[1]);
-	dup2(file[1], STDOUT_FILENO);
-	dup2(fd[0], STDIN_FILENO);
+	close(file[0]);
+	if (dup2(file[1], STDOUT_FILENO) == -1)
+		ft_error("Error en Dup");
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+		ft_error("Error en Dup");
 	close(fd[0]);
-	ft_paths_arg(paths_sep, arg, envp);
+	close(file[1]);
+	cmd = ft_paths_arg(paths_sep, arg, envp);
+	if (!cmd)
+	{
+		free_matrix(paths_sep);
+		ft_putstr_fd("command not found: ", 2);
+		ft_putendl_fd(argv[3], 2);
+		free(cmd);
+	}
+	execve(cmd, arg, envp);
 }
 
 void	ft_pipex(int *fd, char **argv, char **envp)
@@ -72,10 +98,16 @@ void	ft_pipex(int *fd, char **argv, char **envp)
 	file[0] = open(argv[1], O_RDONLY);
 	file[1] = open(argv[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (file[0] == -1 || file[1] == -1)
-		ft_error("Error en File");
+	{
+		perror("Error en File");
+		exit(0);
+	}
 	child_pid = fork();
 	if (child_pid == -1)
-		ft_error("Error en Fork");
+	{
+		perror("Error en Fork");
+		exit(127);
+	}
 	else if (child_pid == 0)
 		ft_child_process(fd, file, argv, envp);
 	else
@@ -88,9 +120,15 @@ int	main(int argc, char **argv, char **envp)
 	int			status;
 
 	if (argc != 5)
-		ft_error("Error en Argc");
+	{
+		ft_putendl_fd("Error en Argc: ./pipex archv1 cm1 cm2 archv2", 2);
+		exit(127);
+	}
 	if (pipe(fd) < 0)
-		ft_error("Error en Pipe");
+	{
+		perror("Error en Pipe");
+		exit(127);
+	}
 	ft_pipex(fd, argv, envp);
 	waitpid(-1, &status, 0);
 }
